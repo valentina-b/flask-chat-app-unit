b9f72ef4c7cab48e57d451908ecfefe63f3ed5d3
valentina-b <valentina.bedi@gmail.com>
2020-05-28 06:45:31 +0000

    Add message list and storing function

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -2,17 +2,22 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route("/")
 def index():
-    """Main page with instructions"""
+    """ Main page with instructions """
     return "To send a message use: /USERNAME/MESSAGE"
 
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    return "Welcome {0}".format(username)
 
 
 @app.route("/<username>/<message>")
