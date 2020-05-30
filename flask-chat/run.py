2b34bc1ee7834a486c2a57e9adce352c7994aa80
valentina-b <valentina.bedi@gmail.com>
2020-05-30 12:29:55 +0000

    Add index.html page

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 messages = []
@@ -20,7 +20,7 @@ def get_all_messages():
 @app.route("/")
 def index():
     """Main page with instructions"""
-    return "To send a message use: /USERNAME/MESSAGE"
+    return render_template("index.html")
 
 
 @app.route("/<username>")
