6501449c3c5f11e4c7b02e5ce6bbcf074c41f999
valentina-b <valentina.bedi@gmail.com>
2020-05-30 15:13:05 +0000

    Add chat.html page

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -30,7 +30,8 @@ def index():
 @app.route("/<username>")
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
 @app.route("/<username>/<message>")
