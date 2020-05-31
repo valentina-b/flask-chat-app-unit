e57756b0be3256e936e17b8d71d1d4312f7b5bdf
valentina-b <valentina.bedi@gmail.com>
2020-05-31 12:22:15 +0000

    Add a textbox

--- flask-chat/run.py
+++ flask-chat/run.py
@@ -27,9 +27,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
     return render_template("chat.html", username=username,
                            chat_messages=messages)
 
